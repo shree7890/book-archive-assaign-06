b3eb70c9330cb257f81df696cabb2da9fe8b6ff3
shree7890 <shreekantadash831@gmail.com>
2021-09-02 18:29:43 +0600

    my commit

--- js/main.js
+++ js/main.js
@@ -13,11 +13,12 @@ const searchBook = () => {
     const searchText = searchField.value;
     // error handling validation
     if (searchText === '') {
-        searchResult.innerHTML = `<h5 class="w-50 mx-auto text-danger">Please! Valid book Name Of Search</h5>`;
+        searchResult.innerHTML = `<h5 class="w-50 mx-auto text-center text-danger bg-white p-2 rounded">Please! Valid book Name Of Search</h5>`;
+        bookShow.innerHTML = '';
     }
     // console.log(searchText);
     else {
-        const url = `http://openlibrary.org/search.json?q=${searchText}`;
+        const url = `https://openlibrary.org/search.json?q=${searchText}`;
         searchField.value = "";
         bookShow.innerHTML = '';
         fetch(url)
@@ -29,21 +30,21 @@ const searchBook = () => {
 const displaySearchResult = books => {
     // console.log(books);
     const arr = books.filter(favourite => favourite.cover_i !== undefined && favourite.author_name !== undefined && favourite.publisher !== undefined && favourite.title !== undefined && favourite.first_publish_year !== undefined);
-    console.log(arr);
+    // console.log(arr);
     // error handling validation
     if (arr.length === 0) {
         bookShow.innerHTML = "";
-        searchResult.innerHTML = `<h5 class="w-50 mx-auto text-danger">No Result Found!</h5>`;
+        searchResult.innerHTML = `<h5 class="w-25 mx-auto text-center text-danger bg-white p-2 rounded">No Result Found!</h5>`;
     }
     else {
         const p = document.createElement("p");
         // book show quantity
-        p.innerHTML = `<h5 class="w-50 mx-auto text-primary">You got ${arr.length} books</h5>`;
+        p.innerHTML = `<h5 class="w-25 mx-auto text-center bg-white p-2 rounded text-primary">You got ${arr.length} books</h5>`;
         bookShow.innerHTML = '';
         bookShow.appendChild(p);
         searchResult.innerHTML = '';
         books.forEach(book => {
-            console.log(book);
+            // console.log(book);
             const div = document.createElement("div");
             div.classList.add("col");
             div.innerHTML = `
@@ -52,9 +53,8 @@ const displaySearchResult = books => {
                 <div class="card-body">
                     <h3 class="card-title">${book.title.slice(0, 20)}</h3>
                     <h4 class="author">${book.author_name[0]}</h4>
-                    <h5 class="publisher text-muted">${book.publisher[0]}</h5>
+                    <h5 class="publisher text-muted">${book.publisher[0].slice(0, 20)}</h5>
                     <p class="year">First Publish Year: ${book.first_publish_year}</p>
-                    
                 </div >
             </div >
     `;
